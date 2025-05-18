e95457c12059570eaa744410782468bbbc9cc075
Kaiz Dawson <kaizdawson@gmail.com>
2025-05-18 15:52:55 +0700

    first push

---
+++ Monopolizers/Models/SignInModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Monopolizers.Models
+{
+    public class SignInModel
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
