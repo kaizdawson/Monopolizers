d56178f005468ec6a426077bfe1c0ecfd8ef1027
Kaiz Dawson <kaizdawson@gmail.com>
2025-06-08 19:05:46 +0700

    cleaner

--- Monopolizers.Services/obj/Debug/net8.0/Monopolizers.Service.AssemblyInfo.cs
+++ Monopolizers.Services/obj/Debug/net8.0/Monopolizers.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Monopolizers.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15e004c9dd7a99d1456db5d756b414b02d787c14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+078b76ce47d34daf1b17395ef58b74dc7e25f932")]
 [assembly: System.Reflection.AssemblyProductAttribute("Monopolizers.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Monopolizers.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
